8f653dc97bf8be3610112c0bf3e15b5e6550d380
Elidion-netizen <sapegaserg@gmail.com>
2024-10-06 14:13:23 +0300

    feat: stream write and cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+const SLICE_NUMBER = 2;
+const PREFIX = "--";
+
 const parseArgs = () => {
-    // Write your code here 
+  const parsedArgs = [];
+
+  process.argv.slice(SLICE_NUMBER).forEach((arg, index) => {
+    if (arg.startsWith(PREFIX)) {
+      const propName = arg.slice(SLICE_NUMBER);
+      const propValue = process.argv[index + PREFIX.length + 1];
+      parsedArgs.push(`${propName} is ${propValue}`);
+    }
+  });
+
+  console.log(parsedArgs.join(", "));
 };
 
 parseArgs();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { createWriteStream } from "node:fs";
+import {
+  FILEDIR,
+  ENCODING as encoding,
+  STREAM_ERRORMSG as ERRORMSG,
+  STREAM_COMPLETE as COMPLETEMSG,
+} from "../utils/constants.js";
+import { EOL } from "node:os";
+import { createFilePath, toGreen } from "../utils/utils.js";
+
+const FILENAME = "fileToWrite.txt";
+const path = createFilePath(import.meta.url, FILEDIR, FILENAME);
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(path, { encoding });
+  process.stdin.on("data", (chunk) => {
+    stream.write(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log(EOL, toGreen(COMPLETEMSG));
+  });
+
+  stream.on("error", () => {
+    console.error(ERRORMSG);
+  });
 };
 
 await write();
