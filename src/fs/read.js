6a81032cf2c0d4911c189042e054b69684e14956
Elidion-netizen <sapegaserg@gmail.com>
2024-10-06 14:13:23 +0300

    feat: fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { readFile } from "node:fs/promises";
+import { createFilePath } from "../utils/utils.js";
+import { FP_ERRORMSG as ERRORMSG, FILEDIR } from "../utils/constants.js";
+
+const FILENAME = "fileToRead.txt";
+
+const path = createFilePath(import.meta.url, FILEDIR, FILENAME);
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(path, {
+      encoding: "utf8",
+    });
+    console.log(content);
+  } catch {
+    throw new Error(ERRORMSG);
+  }
 };
 
 await read();
