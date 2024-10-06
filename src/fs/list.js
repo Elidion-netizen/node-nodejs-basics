858c15222e37c9d373143afd3784aaf6edc63820
Elidion-netizen <sapegaserg@gmail.com>
2024-10-06 14:13:23 +0300

    feat: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir } from "node:fs/promises";
+import { createFilePath } from "../utils/utils.js";
+
+const ERRORMSG = "fs operation failed";
+const path = await createFilePath(import.meta.url, "files");
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const items = await readdir(path);
+    console.log(items);
+  } catch {
+    throw new Error(ERRORMSG);
+  }
 };
 
 await list();
